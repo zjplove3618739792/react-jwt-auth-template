62a48663eef5f135cd9e4ea5e31f2d3626158c81
Steve <zjplove3618739792@gmail.com>
2024-06-25 15:41:19 -0400

    message describing code changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
-// src/App.jsx
-
-import { useState } from 'react';
+import { useState, createContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
+import SignupForm from './components/SignupForm/SignupForm';
+import SigninForm from './components/SigninForm/SigninForm';
+import * as authService from '../src/services/authService'; // import the authservice
+
+export const AuthedUserContext = createContext(null);
 
 const App = () => {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(authService.getUser()); // using the method from authservice
+
+  const handleSignout = () => {
+    authService.signout();
+    setUser(null);
+  };
 
   return (
     <>
-      <NavBar user={user} />
-      <h1>Hello world!</h1>
+      <AuthedUserContext.Provider value={user}>
+        <NavBar user={user} handleSignout={handleSignout} />
+        <Routes>
+          {user ? (
+            <Route path="/" element={<Dashboard user={user} />} />
+          ) : (
+            <Route path="/" element={<Landing />} />
+          )}
+          <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+          <Route path="/signin" element={<SigninForm setUser={setUser} />} />
+        </Routes>
+      </AuthedUserContext.Provider>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,29 @@
-const NavBar = ({ user }) => {
+import { Link } from 'react-router-dom';
+
+// src/components/Navbar/Navbar.jsx
+
+const NavBar = ({ user, handleSignout }) => {
     return (
       <>
-        <p>Hello Navbar</p>
+        { user ? (
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
+            </ul>
+          </nav>
+        ) : (
+          <nav>
+            <ul>
+              {/* <li><Link to="/signin">Sign In</Link></li>
+              <li><Link to="/signup">Sign Up</Link></li> */}
+            </ul>
+          </nav>
+        )}
       </>
     )
   }
-  export default NavBar
   
+  
+
+export default NavBar;
