2104ac2296daaa37c261799e0dec17993bae11f3
Steve <zjplove3618739792@gmail.com>
2024-06-25 15:45:39 -0400

    message describing code changes

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -9,8 +9,7 @@ const Dashboard = ({}) => {
     <main>
       <h1>Welcome, {user.username}</h1>
       <p>
-        This is the dashboard page where you, and only you, can see a dashboard
-        of all of your things.
+        There is nothing here, dumbass.
       </p>
     </main>
   );
